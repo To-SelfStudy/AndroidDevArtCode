8915cb14647e6dee209adbf0dc207b6d0d912cb1
sundong <sundong@yingu.com>
2017-02-20 17:48:59 +0800

    just commit

--- ipcdemo/src/main/java/com/lewish/start/ipcdemo/activity/BookManagerActivity.java
+++ ipcdemo/src/main/java/com/lewish/start/ipcdemo/activity/BookManagerActivity.java
@@ -6,16 +6,33 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.lewish.start.ipcdemo.IBookManager;
 import com.lewish.start.ipcdemo.R;
+import com.lewish.start.ipcdemo.entity.Book;
 import com.lewish.start.ipcdemo.service.BookManagerService;
 
+import java.util.List;
+
 public class BookManagerActivity extends AppCompatActivity {
+    private static final String TAG = "BookManagerActivity";
     private ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            IBookManager bookManager = IBookManager.Stub.asInterface(iBinder);
 
+            try {
+                List<Book> bookList = bookManager.getBookList();
+                for (int i=0;i<bookList.size();i++){
+                    Book book = bookList.get(i);
+                    Log.d(TAG, "BookId="+book.bookId+",BookName="+book.bookName);
+                }
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -29,5 +46,11 @@ public class BookManagerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_book_manager);
         Intent intent = new Intent(this, BookManagerService.class);
         bindService(intent,serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onDestroy() {
+        unbindService(serviceConnection);
+        super.onDestroy();
     }
 }
